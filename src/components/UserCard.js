0f8cfe1d1a5dda856263fef83219063a9acc4fee
gunsaman <heaven.gunsa@gmail.com>
2021-04-11 21:55:12 +0300

    improve UI with built in MaterialUI component instead of custom css styling

--- src/components/UserCard.js
+++ src/components/UserCard.js
@@ -2,19 +2,70 @@ import React from 'react';
 import '../App.css';
 import ButtonComponent from './ButtonComponent';
 import { Link } from 'react-router-dom';
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import Avatar from '@material-ui/core/Avatar';
+import Typography from '@material-ui/core/Typography';
 
-export default function UserCard(props) {
+const useStyles = makeStyles({
+    root: {
+      maxWidth:230,
+      minWidth: 200,
+      minHeight:250,
+      textAlign:'center'
+    },
+ 
+    bullet: {
+      display: 'inline-block',
+      margin: '0 2px',
+      transform: 'scale(0.8)',
+    },
     
+    title: {
+      fontSize: 12,
+    },
+    pos: {
+      marginBottom: 12,
+    },
+  }); 
+export default function UserCard(props) {
+    const classes = useStyles();
+  
     return(
        
-        <div className="user-cards">
-            <div className="card-icon">{props.name.charAt(0)}</div>
+        <div>
+            <Card className={classes.root}>
+                <CardContent>
+                    <Typography className={classes.title} color="textSecondary" gutterBottom>
+                    <Avatar className= "card-icon" variant="circular">{props.name.charAt(0)}</Avatar>
+                    </Typography>
+                    <Typography variant="subtitle2">
+                    {props.name}
+                    </Typography>
+                    <Typography variant="subtitle2" className={classes.pos} color="textSecondary">
+                    @{props.username}
+                    </Typography>
+                    <Typography variant="subtitle2" component="p">
+                    <a href={props.website}>https://{props.website}</a>
+                    <br />
+                    
+                    </Typography>
+                </CardContent>
+                <CardActions className="detailBtn">
+                     <Link to={`/user/${props.id}`} style={{textDecoration:"none"}}><ButtonComponent/> </Link>
+                </CardActions>
+            </Card>
+            {/* <div className="card-icon">
+                <Avatar>{props.name.charAt(0)}</Avatar>
+            </div>
             <div className="card-body">
                 <div className="title">{props.name}</div>
                 <div className="main-content">@{props.username}</div>
                 <div className="main-content"><a href={props.website}>https://{props.website}</a></div>
             </div>
-            <Link to={`/user/${props.id}`} style={{textDecoration:"none"}}><ButtonComponent/> </Link>
+            <Link to={`/user/${props.id}`} style={{textDecoration:"none"}}><ButtonComponent/> </Link> */}
         </div>
     )
 }
--- src/components/UserDetail.js
+++ src/components/UserDetail.js
@@ -1,28 +1,108 @@
 import React from 'react';
 import {useParams} from 'react-router-dom';
+import Divider from '@material-ui/core/Divider';
+import { TelephoneFill, Envelope, Building, Globe, GeoAltFill, PersonBadge } from 'bootstrap-icons-react';
+import Card from '@material-ui/core/Card';
+import { makeStyles } from '@material-ui/core/styles';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import Typography from '@material-ui/core/Typography';
+
 import '../App.css';
 
+
+const useStyles = makeStyles({
+    root: {
+      minWidth: 275,
+      textAlign:'center'
+    },
+ 
+    bullet: {
+      display: 'inline-block',
+      margin: '0 2px',
+      transform: 'scale(0.8)',
+    },
+    
+    title: {
+      fontSize: 14,
+    },
+    pos: {
+      marginBottom: 12,
+    },
+  }); 
 export default function UserDetail({data}){
     const users = [{data}];
+    const classes = useStyles();
     console.log(users)
     const {id} = useParams();
     const  fullUserDetail = (users[0].data.filter(user => user.id === Number(id)));
     
     return(
-        <div className="full-card">
+        <Card className={classes.root}>
+        <CardContent>
+            <div className="card-title">
+                <Typography className={classes.title} color="textPrimary" className={classes.pos}>
+                    <h1>{fullUserDetail[0].name}</h1>
+                    
+                </Typography>
+                
+            </div>
+            <Divider variant="middle" />
+            <Typography variant="h6" className={classes.pos}>
+            <PersonBadge  />Username: @{fullUserDetail[0].username}
+            </Typography>
+
+            <Typography variant="subtitle2" className={classes.pos} color="textSecondary">
+            <Envelope /> Email: {fullUserDetail[0].email}
+            </Typography>
+
+            <Typography  variant="subtitle2" color="textSecondary" className={classes.pos}>
+            <Building  /> Company: {fullUserDetail[0].company.name}
+            </Typography>
+
+            <Typography variant="subtitle2">
+             <TelephoneFill />  {fullUserDetail[0].phone}
+            <Typography>
+            <div className="address">
+                <ul>
+                    <GeoAltFill /> Address:
+                        <li>Street:{fullUserDetail[0].address.street}</li>
+                        <li>Suite:{fullUserDetail[0].address.suite}</li>
+                        <li>Zipcode:{fullUserDetail[0].address.zipcode}</li>
+                        <li>City:{fullUserDetail[0].address.city}</li>
+                        
+                </ul> 
+            </div>
+            </Typography>
+            <br />
+            
+            </Typography>
+        </CardContent>
+        <CardActions className="detailBtn">
+            
+        </CardActions>
+    </Card>
+       /* { <div className="full-card">
+            <div className="card-content">
             <h1>{fullUserDetail[0].name}</h1>
-            <p>-username: @{fullUserDetail[0].username}</p>
-            <p>-email: {fullUserDetail[0].email}</p>
-            <p>-phone: {fullUserDetail[0].phone}</p>
-            <p>-company: {fullUserDetail[0].company.name}</p>
-            <p>-website: http://{fullUserDetail[0].website}</p>
-            <p>-address: </p>
+            <Divider variant="middle" />
+            <p><PersonBadge  />Username: @{fullUserDetail[0].username}</p>
+           <p> <Envelope /> Email: {fullUserDetail[0].email}</p>
+            <p> <TelephoneFill />  {fullUserDetail[0].phone}</p>
+            <p><Building  /> Company: {fullUserDetail[0].company.name}</p>
+            <p><Globe  />  Website: http://{fullUserDetail[0].website}</p>
             <ul>
-                <li>street:{fullUserDetail[0].address.street}</li>
-                <li>suite:{fullUserDetail[0].address.suite}</li>
-                <li>city:{fullUserDetail[0].address.city}</li>
-                <li>zipcode:{fullUserDetail[0].address.zipcode}</li>
-            </ul>                                                
-        </div>
+                <p><GeoAltFill /> Address: </p>
+                    <li>Street:{fullUserDetail[0].address.street}</li>
+                    <li>Suite:{fullUserDetail[0].address.suite}</li>
+                    <li>Zipcode:{fullUserDetail[0].address.zipcode}</li>
+                    <li>City:{fullUserDetail[0].address.city}</li>
+                    
+            </ul> 
+            <div>
+            
+            </div>   
+            </div>        }                                     
+        </div>*/
     )
 }
